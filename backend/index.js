d8c7218d0bcd9874f55fe11cd15328546022c725
mohitdhiman80 <mohitdhiman.hcl@gmail.com>
2026-01-12 18:56:25 +0530

    ad

--- backend/index.js
+++ backend/index.js
@@ -5,17 +5,38 @@ const app = express();
 const PORT = 5000;
 
 // Prometheus metrics
-const collectDefaultMetrics = client.collectDefaultMetrics;
-collectDefaultMetrics();
+client.collectDefaultMetrics();
 
 const httpRequestsTotal = new client.Counter({
   name: "http_requests_total",
   help: "Total number of HTTP requests"
 });
 
+// CPU stress function
+function burnCPU(ms) {
+  const end = Date.now() + ms;
+  while (Date.now() < end) {
+    Math.sqrt(Math.random());
+  }
+}
+
 app.get("/", (req, res) => {
   httpRequestsTotal.inc();
   res.json({ message: "Backend is running 🚀" });
+});
+
+app.get("/stress", (req, res) => {
+  httpRequestsTotal.inc();
+
+  const level = req.query.level || "low";
+  let duration = 0;
+
+  if (level === "low") duration = 300;
+  if (level === "medium") duration = 1500;
+  if (level === "high") duration = 4000;
+
+  burnCPU(duration);
+  res.send(`CPU stress applied: ${level}`);
 });
 
 app.get("/metrics", async (req, res) => {
